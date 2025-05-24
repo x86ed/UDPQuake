8f1c960cdc89b97253b1b132fd8fdd12e95dfb94
Adam Siegel <tobioatom@gmail.com>
2025-05-24 14:03:35 -0700

    versioning nonsense

--- UDPQuake/__init__.py
+++ UDPQuake/__init__.py
@@ -1,1 +1,3 @@
 # This file makes the UDPQuake directory a Python package
+"""UDPQuake - USGS Earthquake Monitor for Meshtastic Networks"""
+__version__ = "0.1.0"
--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,1 @@
 # This file makes the test directory a Python package
-"""UDPQuake - USGS Earthquake Monitor for Meshtastic Networks"""
-__version__ = "0.1.0"
